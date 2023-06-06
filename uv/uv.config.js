a88e9f637a22b6979b48629d84fde8125e7624a6
Skirt <126694961+skirtsnetwork@users.noreply.github.com>
2023-06-06 15:13:42 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://incog.dev/bare/",
+  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
